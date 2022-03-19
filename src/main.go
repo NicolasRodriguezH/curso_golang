9dfd13dad1656e22a32e09779a4100fddac0809e
Nicolas <nicolashilarion420@gmail.com>
2022-03-19 13:17:03 -0500

    For init interfaces

--- src/main.go
+++ src/main.go
@@ -1,19 +1,5 @@
 package main
 
-import "fmt"
-
-// pc pc para definir computadores
-type pc struct {
-	ram   int
-	disk  int
-	marca string
-}
-
-func (myCp pc) String() string {
-	return fmt.Sprintf("Tengo %d GB RAM, %d GB DISK y es una %s", myCp.ram, myCp.disk, myCp.marca)
-}
-
 func main() {
-	myPc := pc{ram: 16, disk: 121, marca: "acme"}
-	fmt.Println(myPc)
+
 }
