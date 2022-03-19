f514819556b68525904f60dd1f08d6985d1de3b0
Nicolas <nicolashilarion420@gmail.com>
2022-03-19 14:06:43 -0500

    interfaces list interfaces finalized

--- src/main.go
+++ src/main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+type figuras2D interface {
+	area() float64
+}
+
+type cuadrado struct {
+	base float64
+}
+
+type rectangulo struct {
+	base   float64
+	altura float64
+}
+
+func (c cuadrado) area() float64 {
+	return c.base * c.base
+}
+
+func (r rectangulo) area() float64 {
+	return r.base * r.altura
+}
+
+func calcular(f figuras2D) {
+	fmt.Println("Area: ", f.area())
+}
+
+func main() {
+	myCuadradein := cuadrado{base: 2}
+	myRectangulein := rectangulo{base: 2, altura: 4}
+
+	calcular(myRectangulein)
+	calcular(myCuadradein)
+
+	// Lista interfaces
+	myInterface := []interface{}{"Hola", 12, 4.89}
+	fmt.Println(myInterface...)
 }
