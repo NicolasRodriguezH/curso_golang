3211aaef50dcd8bb7a1333fab6331a1be88f9be6
Nicolas <nicolashilarion420@gmail.com>
2022-03-22 20:39:33 -0500

    lista para pruebas

--- src/main.go
+++ src/main.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	/*
 
-		// Instanciar echo
-		e := echo.New()
-
-		// Ruta
-		e.GET("/", func(c echo.Context) error {
-			return c.String(http.StatusOK, "HELLO WORD")
-		})
-		e.Logger.Fatal(e.Start(":1323"))
-
-	*/
-	fmt.Println("bre")
 }
