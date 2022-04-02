1e19918db185e1c43ca62266a184d3db2ee4a820
Nicolas <nicolashilarion420@gmail.com>
2022-04-01 19:02:09 -0500

    Basic calculator from console

--- src/main.go
+++ src/main.go
@@ -1,5 +1,39 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	operacion := scanner.Text()
+	fmt.Println(operacion)
+	operador := "/"
+	valores := strings.Split(operacion, operador)
+	fmt.Println(valores)
+	fmt.Println(valores[0] + valores[1])
+	operador1, err1 := strconv.Atoi(valores[0])
+	if err1 != nil {
+		fmt.Println(err1)
+	} else {
+		fmt.Println(operador1)
+	}
+	operador2, _ := strconv.Atoi(valores[1])
+	switch operador {
+	case "+":
+		fmt.Println(operador1 + operador2) // <-Este operador es el que ejecuta la operacion realmente
+	case "-":
+		fmt.Println(operador1 - operador2)
+	case "*":
+		fmt.Println(operador1 * operador2)
+	case "/":
+		fmt.Println(operador1 / operador2)
+	default:
+		fmt.Println("Valor no reconocido")
+	}
 }
