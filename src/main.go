d94721e270a629fa3b2ce0a1fede71e8d58622c4
Nicolas <nicolashilarion420@gmail.com>
2022-05-04 12:48:23 -0500

    fmt-convertions-arrowStringLiteral

--- src/main.go
+++ src/main.go
@@ -1,5 +1,30 @@
 package main
 
+import "fmt"
+
+var a int
+
+type num int
+
+var p num
+
 func main() {
 
+	s := `Este seria un 
+	arrow 
+	string 
+	literal`
+
+	fmt.Println(s)
+	fmt.Printf("%T\n\n", s)
+
+	a = 42
+	fmt.Println(a)
+	fmt.Printf("%d\t%b\t%#x\n\n", a, a, a)
+
+	p = 34
+	fmt.Println(p)
+	a = int(p)
+	fmt.Printf("%T\n", a)
+	fmt.Println(a)
 }
