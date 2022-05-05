281a9b1d755cda241ccdd1bcd021113d30b21f08
Nicolas <nicolashilarion420@gmail.com>
2022-05-05 16:55:02 -0500

    Print years lived

--- src/main.go
+++ src/main.go
@@ -2,25 +2,12 @@ package main
 
 import "fmt"
 
+//Imprime los años vividos
 func main() {
-	a := (42 == 42)
-	b := (42 <= 42)
-	c := (42 >= 42)
-	d := (42 != 42)
-	e := (42 > 42)
-	f := (42 < 42)
-
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Println(d)
-	fmt.Println(e)
-	fmt.Println(f)
-
-	for i := 65; i <= 90; i++ {
-		fmt.Println(i)
-		for j := 0; j < 3; j++ {
-			fmt.Printf("\t%#U\n", i)
-		}
+	bi := 1997
+	for bi <= 2022 {
+		fmt.Println(bi)
+		bi++
 	}
+
 }
