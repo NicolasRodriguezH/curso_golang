b9020883d2ba3bf38e1dda1181313e60176ad4fa
Nicolas <nicolashilarion420@gmail.com>
2022-05-09 18:00:29 -0500

    Slices append and delete elements

--- src/main.go
+++ src/main.go
@@ -3,33 +3,18 @@ package main
 import "fmt"
 
 func main() {
-
-	//Arrays no recomended
-	var x [5]int
-	x[3] = 21
-	fmt.Println(x)
-	fmt.Printf("%T\n", x)
-	fmt.Println(len(x))
-
 	//Slices
 	//Tipo{elementos} //COMPOSITE LITERAL
 	s := []int{1, 2, 3, 4, 5}
 	fmt.Println(s)
-	fmt.Printf("%T\n", s)
-	fmt.Println(s[0])
+	s = append(s, 6, 7, 8)
+	fmt.Println(s)
 
-	fmt.Println(s[1:3])
-	fmt.Println(s[2:4])
-	fmt.Println(len(s))
-	fmt.Println(cap(s))
+	a := []int{9, 10}
+	s = append(s, a...)
+	fmt.Println(s)
 
-	//Range slice
-	for i, v := range s {
-		fmt.Println(i, " ", v)
-	}
-
-	//Clause for slice
-	for i := 0; i < len(s); i++ {
-		fmt.Println(i, s[i])
-	}
+	//Eliminar elementeos del slice
+	s = append(s[:5], s[7:]...)
+	fmt.Println(s)
 }
