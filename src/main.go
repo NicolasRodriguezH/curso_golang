f94591e61c1fb0e346d8f3ee30d8d83458bd7314
Nicolas <nicolashilarion420@gmail.com>
2022-05-09 16:49:32 -0500

    Practice slices

--- src/main.go
+++ src/main.go
@@ -3,44 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	//Imprime los años vividos con for infinito y break
-	bi := 1997
-	for {
-		fmt.Println(bi)
-		bi++
-		if bi == 2022 {
-			break
-		}
+
+	//Arrays no recomended
+	var x [5]int
+	x[3] = 21
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+	fmt.Println(len(x))
+
+	//Slices
+	//Tipo{elementos} //COMPOSITE LITERAL
+	s := []int{1, 2, 3, 4, 5}
+	fmt.Println(s)
+	fmt.Printf("%T\n", s)
+	fmt.Println(s[0])
+
+	fmt.Println(s[1:3])
+	fmt.Println(s[2:4])
+	fmt.Println(len(s))
+	fmt.Println(cap(s))
+
+	//Range slice
+	for i, v := range s {
+		fmt.Println(i, " ", v)
 	}
 
-	//Imprime el modulo de 4 en rango de 10 a 100
-	for n := 10; n <= 100; n++ {
-		fmt.Printf("Si dividimos %v ebtre 4, el resto (tambien modulo) es %v\n", n, n%4)
-	}
-
-	//Imprime la validacion de un if
-	x := 21
-	if x != 22 {
-		fmt.Println("El if funciona correctamente")
-		fmt.Println(x)
-	}
-
-	//Un switch sin expresion especificada
-	switch {
-	case false:
-		fmt.Println("No deberia imprimir")
-	case true:
-		fmt.Println("Deberia imprimir")
-	}
-
-	//Un switch con expresion, le estaba agregando var en la asignacion a la variable y no es necesario ya que esta en scope a nivel de paquete
-	deporteFav := "Fitness"
-	switch deporteFav {
-	case "Baseball":
-		fmt.Println("Ve al estadio")
-	case "Workout":
-		fmt.Println("En la casa")
-	case "Fitness":
-		fmt.Println("Go to the fitness")
+	//Clause for slice
+	for i := 0; i < len(s); i++ {
+		fmt.Println(i, s[i])
 	}
 }
